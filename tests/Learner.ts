e5c52884a942b9ff616b68c83ef94d561883cc33
Isma Navarro <ismanapa@gmail.com>
2022-06-03 12:06:07 +0200

    feat: add to primitives method to test classes

--- tests/Learner.ts
+++ tests/Learner.ts
@@ -1,5 +1,12 @@
+import { Primitives } from "../src/Primitives";
 import { CourseId } from "./CourseId";
 
 export class Learner {
   constructor(readonly enrolledCourses: CourseId[]) {}
+
+  toPrimitives(): Primitives<Learner> {
+    return {
+      enrolledCourses: this.enrolledCourses.map((course) => course.value),
+    };
+  }
 }
