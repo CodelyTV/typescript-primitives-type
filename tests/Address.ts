5f8c837089ac8afc283fd0a3adb9d1f0ce758b2b
Isma Navarro <ismanapa@gmail.com>
2022-06-03 12:06:07 +0200

    feat: add to primitives method to test classes

--- tests/Address.ts
+++ tests/Address.ts
@@ -1,6 +1,14 @@
+import { Primitives } from "../src/Primitives";
 import { City } from "./City";
 import { Street } from "./Street";
 
 export class Address {
   constructor(readonly street: Street, readonly city: City) {}
+
+  toPrimitives(): Primitives<Address> {
+    return {
+      street: this.street.value,
+      city: this.city.value,
+    };
+  }
 }
--- tests/User.ts
+++ tests/User.ts
@@ -1,5 +1,12 @@
+import { Primitives } from "../src/Primitives";
 import { Address } from "./Address";
 
 export class User {
   constructor(readonly address: Address) {}
+
+  toPrimitives(): Primitives<User> {
+    return {
+      address: this.address.toPrimitives(),
+    };
+  }
 }
