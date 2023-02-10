b3e5364661961040fd6400ef33784c99727c1a50
arnold095 <54068813+arnold095@users.noreply.github.com>
2023-02-10 11:38:33 +0100

    feat: Add support for primitives and undefined values
    * test: add more test scenarios
    
    * feat: add support to more types
    
    * restore: restore last version

--- tests/Primitives.test.ts
+++ tests/Primitives.test.ts
@@ -1,10 +1,12 @@
 import { expectTypeOf } from "expect-type";
 
-import { Primitives } from "../src/Primitives";
+import { Primitives } from "../src";
 import { Course } from "./Course";
 import { DeliveryInfo } from "./DeliveryInfo";
 import { Learner } from "./Learner";
+import { Product } from "./Product";
 import { User } from "./User";
+import { Video } from "./Video";
 
 describe("Primitives", () => {
   it("should ensure to only return primitive properties excluding methods", () => {
@@ -48,6 +50,29 @@ describe("Primitives", () => {
         readonly city: string;
         readonly street: string;
       }[];
+    };
+
+    expectTypeOf<actualPrimitives>().toEqualTypeOf<expectedPrimitives>();
+  });
+
+  it("should get primitive type in case it is not a value object", () => {
+    type actualPrimitives = Primitives<Product>;
+
+    type expectedPrimitives = {
+      readonly active: boolean;
+      readonly createdAt: Date;
+    };
+
+    expectTypeOf<actualPrimitives>().toEqualTypeOf<expectedPrimitives>();
+  });
+
+  it("should infer the optional properties", () => {
+    type actualPrimitives = Primitives<Video>;
+
+    type expectedPrimitives = {
+      readonly id: string;
+      readonly name: string | undefined;
+      readonly duration: number | undefined;
     };
 
     expectTypeOf<actualPrimitives>().toEqualTypeOf<expectedPrimitives>();
---
+++ tests/Video.ts
@@ -0,0 +1,19 @@
+import { Primitives } from "../src";
+import { VideoId } from "./VideoId";
+import { VideoName } from "./VideoName";
+
+export class Video {
+  public constructor(
+    public readonly id: VideoId,
+    public readonly name: VideoName | undefined,
+    public readonly duration: number | undefined
+  ) {}
+
+  public toPrimitives(): Primitives<Video> {
+    return {
+      id: this.id.value,
+      name: this.name?.value,
+      duration: this.duration,
+    };
+  }
+}
