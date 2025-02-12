df0723de02e807f4df8161b98a545d99ff0c589e
Rafa Gómez <rgomezcasas@gmail.com>
2025-02-12 15:35:52 +0100

    feat!: convert date to iso instead of number
    Since it's a more standard approach

--- src/Primitives.ts
+++ src/Primitives.ts
@@ -1,4 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-function-type */
+type Year = `${number}${number}${number}${number}`;
+type Month = `${number}${number}`;
+type Day = `${number}${number}`;
+type Hour = `${number}${number}`;
+type Minute = `${number}${number}`;
+type Second = `${number}${number}`;
+type Milliseconds = `${number}${number}${number}`;
+type Timezone = "Z" | `${"+" | "-"}${number}${number}:${number}${number}`;
+
+export type ISODateTime =
+	`${Year}-${Month}-${Day}T${Hour}:${Minute}:${Second}.${Milliseconds}${Timezone}`;
+
 type Methods<T> = {
 	[P in keyof T]: T[P] extends Function ? P : never;
 }[keyof T];
@@ -9,16 +21,16 @@ type Properties<T> = Omit<MethodsAndProperties<T>, Methods<T>>;
 
 type PrimitiveTypes = string | number | boolean | undefined | null;
 
-type DateToNumber<T> = T extends Date ? number : T;
+type DateToISODateTime<T> = T extends Date ? ISODateTime : T;
 
 type ValueObjectValue<T> = T extends PrimitiveTypes
 	? T
 	: T extends Date
-		? number
+		? ISODateTime
 		: T extends { value: infer U }
-			? DateToNumber<U>
+			? DateToISODateTime<U>
 			: T extends Array<{ value: infer U }>
-				? DateToNumber<U>[]
+				? DateToISODateTime<U>[]
 				: T extends Array<infer U>
 					? Array<ValueObjectValue<U>>
 					: T extends { [K in keyof Properties<T>]: unknown }
@@ -26,7 +38,7 @@ type ValueObjectValue<T> = T extends PrimitiveTypes
 								[K in keyof Properties<T>]: ValueObjectValue<Properties<T>[K]>;
 							}
 						: T extends unknown
-							? DateToNumber<T>
+							? DateToISODateTime<T>
 							: never;
 
 export type Primitives<T> = {
